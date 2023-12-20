d444b20e0e3054d59b41d32d78750dca0588847c
ToyFarms <diandrashafarrahman1@gmail.com>
2023-12-21 03:00:19 +0700

    Factor out os specific code, it should be cross platform now (once its implemented that is)

---
+++ src/interface/libos.h
@@ -0,0 +1,12 @@
+#ifndef _LIBOS_H
+#define _LIBOS_H
+
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+#define AP_WINDOWS
+#elif __APPLE__
+#define AP_MACOS
+#elif __linux__
+#define AP_LINUX
+#endif // WIN32
+
+#endif // _LIBOS_H
--- src/windows/libcli.c
+++ src/windows/libcli.c
@@ -23,7 +23,7 @@ CLIState *cli_state_init()
 
     cst->force_redraw = false;
 
-    cst->out = NULL;
+    cst->out.handle = NULL;
     cst->width = 0;
     cst->width = 0;
 }
@@ -62,7 +62,7 @@ static void _cli_get_console_size(HANDLE out, int *out_width, int *out_height)
 
 void cli_get_console_size(CLIState *cst)
 {
-    _cli_get_console_size(cst->out, &cst->width, &cst->height);
+    _cli_get_console_size(cst->out.handle, &cst->width, &cst->height);
 }
 
 void cli_clear_screen(HANDLE out)
@@ -89,7 +89,7 @@ typedef enum LineState
 static StringBuilder *sb;
 static wchar_t strw[2048];
 
-static wchar_t *_cli_line_routine(CLIState *cst, int idx, LineState line_state, int *out_strlen)
+static wchar_t *cli_line_routine(CLIState *cst, int idx, LineState line_state, int *out_strlen)
 {
     sb_appendf(sb, "\x1b[0m%d ", idx);
     switch (line_state)
@@ -119,7 +119,7 @@ static wchar_t *_cli_line_routine(CLIState *cst, int idx, LineState line_state, 
     return strw;
 }
 
-static LineState _cli_get_line_state(CLIState *cst, int idx)
+static LineState cli_get_line_state(CLIState *cst, int idx)
 {
     if (idx == cst->playing_idx)
         return LINE_PLAYING;
@@ -140,24 +140,24 @@ static void _cli_draw(CLIState *cst)
     LineState line_state;
 
     if (cst->force_redraw)
-        cli_clear_screen(cst->out);
+        cli_clear_screen(cst->out.handle);
 
     for (int viewport_offset = 0;
          viewport_offset < cst->height && cst->entry_offset + viewport_offset < cst->entry_size;
          viewport_offset++)
     {
         int abs_entry_idx = cst->entry_offset + viewport_offset;
-        line_state = _cli_get_line_state(cst, abs_entry_idx);
+        line_state = cli_get_line_state(cst, abs_entry_idx);
 
         if (!cst->force_redraw && lines_state_cache[abs_entry_idx] && line_state == lines_state_cache[abs_entry_idx])
             continue;
 
         lines_state_cache[abs_entry_idx] = line_state;
 
-        SetConsoleCursorPosition(cst->out, (COORD){0, viewport_offset});
+        SetConsoleCursorPosition(cst->out.handle, (COORD){0, viewport_offset});
 
-        str = _cli_line_routine(cst, abs_entry_idx, line_state, &str_len);
-        WriteConsoleW(cst->out, str, str_len, NULL, NULL);
+        str = cli_line_routine(cst, abs_entry_idx, line_state, &str_len);
+        WriteConsoleW(cst->out.handle, str, str_len, NULL, NULL);
     }
 }
 
@@ -184,7 +184,7 @@ static HANDLE out_cur;
 static HANDLE in;
 static DWORD in_main_mode;
 
-static void _cli_init_input()
+static void cli_init_input()
 {
     in = CreateFile("CONIN$",
                     GENERIC_READ | GENERIC_WRITE,
@@ -222,7 +222,7 @@ void cli_buffer_switch(BUFFER_TYPE type)
                                   OPEN_ALWAYS,
                                   0,
                                   NULL);
-        
+
         if (in && in_main_mode)
             SetConsoleMode(in, in_main_mode);
 
@@ -249,7 +249,7 @@ void cli_buffer_switch(BUFFER_TYPE type)
         out_alt_mode &= ~ENABLE_WRAP_AT_EOL_OUTPUT;
         SetConsoleMode(out_alt, out_alt_mode);
 
-        _cli_init_input();
+        cli_init_input();
 
         SetConsoleActiveScreenBuffer(out_alt);
 
@@ -260,26 +260,85 @@ void cli_buffer_switch(BUFFER_TYPE type)
 }
 
 static INPUT_RECORD in_rec[128];
+static Event in_event[128];
 
-INPUT_RECORD *cli_read_in(unsigned long *out_size)
+Events cli_read_in()
 {
     if (!in)
     {
         av_log(NULL, AV_LOG_FATAL, "Input is uninitialized.\n");
-        return NULL;
+        return (Events){.event = NULL, .event_size = 0};
     }
 
     ULONG read;
     BOOL exit;
     ReadConsoleInput(in, in_rec, sizeof(in_rec) / sizeof(INPUT_RECORD), &read);
 
-    if (out_size)
-        *out_size = read;
+    for (int i = 0; i < read; i++)
+    {
+        INPUT_RECORD e = in_rec[i];
+        KEY_EVENT_RECORD k = e.Event.KeyEvent;
+        MOUSE_EVENT_RECORD m = e.Event.MouseEvent;
+        WINDOW_BUFFER_SIZE_RECORD b = e.Event.WindowBufferSizeEvent;
 
-    return in_rec;
+        EventType type;
+        KeyEvent key_event;
+        MouseEvent mouse_event;
+        BufferChangedEvent buf_event;
+
+        switch (e.EventType)
+        {
+        case KEY_EVENT:
+            type = KEY_EVENT_TYPE;
+
+            key_event.key_down = k.bKeyDown;
+            key_event.acsii_key = k.uChar.AsciiChar;
+            key_event.unicode_key = k.uChar.UnicodeChar;
+            key_event.vk_key = k.wVirtualKeyCode;
+
+            DWORD ks = k.dwControlKeyState;
+            key_event.modifier_key = (ks & SHIFT_PRESSED << 0) | (ks & LEFT_CTRL_PRESSED << 1) | (ks & RIGHT_CTRL_PRESSED << 1) | (ks & LEFT_ALT_PRESSED << 2) | (ks & RIGHT_ALT_PRESSED << 2);
+
+            break;
+        case MOUSE_EVENT:
+            type = MOUSE_EVENT_TYPE;
+
+            mouse_event.x = m.dwMousePosition.X;
+            mouse_event.y = m.dwMousePosition.Y;
+            DWORD ms = m.dwButtonState;
+            mouse_event.state = (ms & FROM_LEFT_1ST_BUTTON_PRESSED << 0) | (ms & FROM_LEFT_2ND_BUTTON_PRESSED << 1) | (ms & FROM_LEFT_3RD_BUTTON_PRESSED << 2);
+            mouse_event.double_clicked = m.dwEventFlags == DOUBLE_CLICK;
+            mouse_event.moved = m.dwEventFlags & MOUSE_MOVED;
+            mouse_event.scrolled = m.dwEventFlags & MOUSE_WHEELED;
+            mouse_event.scroll_delta = GET_WHEEL_DELTA_WPARAM(m.dwButtonState);
+
+            break;
+        case WINDOW_BUFFER_SIZE_EVENT:
+            type = BUFFER_CHANGED_EVENT_TYPE;
+            buf_event.width = b.dwSize.X;
+            buf_event.height = b.dwSize.Y;
+
+            break;
+        default:
+            type = UNKNOWN_EVENT_TYPE;
+            break;
+        }
+
+        if (type == UNKNOWN_EVENT_TYPE)
+            continue;
+
+        in_event[i] = (Event){
+            .type = type,
+            .key_event = key_event,
+            .mouse_event = mouse_event,
+            .buf_event = buf_event,
+        };
+    }
+
+    return (Events){.event = in_event, .event_size = read};
 }
 
-HANDLE cli_get_handle(BUFFER_TYPE type)
+Handle cli_get_handle(BUFFER_TYPE type)
 {
     HANDLE ret;
     switch (type)
@@ -295,7 +354,7 @@ HANDLE cli_get_handle(BUFFER_TYPE type)
     if (!ret)
     {
         av_log(NULL, AV_LOG_FATAL, "Output handle is uninitialized.\n");
-        return NULL;
+        return (Handle){.handle = NULL};
     }
-    return ret;
+    return (Handle){.handle = ret};
 }
